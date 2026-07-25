a087132dd610be09e2359753a92d34802ca206b3
4A-17\koyo <a>
2026-07-25 11:20:47 +0900

    DxLibの追加とゲームループの実装

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,60 @@
-int Main()
+// =======================================
+//  ライブラリ読込
+// =======================================
+#include "Game.h"
+#include "DxLib.h"
+
+//	定数の宣言
+#define WINDOW_W 1280	// 画面サイズ　横
+#define WINDOW_H 720	// 画面サイズ　縦
+
+// ------------------------------
+// WinMain
+// ------------------------------
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
+
+	// =======================================
+	//  初期化処理
+	// =======================================
+	// ログの書き出しを無効化
+	SetOutApplicationLogValidFlag(FALSE);	// ログの書き出しを無効化
+
+	// ウィンドウモードの設定
+	ChangeWindowMode(TRUE);					// ウインドウモードを有効化
+
+	// ウィンドウサイズとカラービットの設定
+	SetGraphMode(WINDOW_W, WINDOW_H, 32);
+
+	// ウィンドウのタイトル
+	SetMainWindowText("DxLib Template");
+
+	// 背景色の設定
+	SetBackgroundColor(128, 128, 128);
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1)
+	{
+		return -1;							// エラーが起きたら直ちに終了
+	}
+
+	// ウィンドウ描画モード設定
+	SetDrawScreen(DX_SCREEN_BACK);			// 裏画面に描画する
+
+
+	// ゲームクラスのインスタンスを作成
+	Game game_obj;
+
+	// ゲームループを呼び出す
+	game_obj.GameLoop();
+
+
+	// =======================================
+	//  終了処理
+	// =======================================
+	// ＤＸライブラリ使用の終了処理
+	DxLib_End();							// ＤＸライブラリ使用の終了処理
+
+	// ソフトの終了
 	return 0;
 }

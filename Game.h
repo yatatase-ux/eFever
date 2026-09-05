69cef600a7cf8410474c693c652bc1105d0cf1a2
4A-16\koyo <a>
2026-09-05 09:50:58 +0900

    ひとまずOPが表示されるように設計

--- Game.h
+++ Game.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <ctime>		// 標準C ライブラリ ヘッダー <time.h> をインクルードし、関連する名前を std 名前空間に追加します。
 #include "DxLib.h"		// DxLib
+#include "SceneManager.h"
 
 class Game
 {
-protected:
+private:
+
+	SceneManager SM;
 
 public:
 

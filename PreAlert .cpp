5f6294fb47b157633ded68c62d90d97a6325a28e
4A-17\koyo <a>
2026-09-19 11:41:40 +0900

    Preに先バレが始まる関数を追加

--- PreAlert .cpp
+++ PreAlert .cpp
@@ -5,6 +5,12 @@ void PreAlert::Init()
 {
     pre = 20;
     pre_img = LoadGraph("image/pre.png");
+}
+
+void PreAlert::Start()
+{
+    active = true;
+    end = false;
     pre_posy = -300;
 }
 
@@ -18,14 +24,27 @@ bool PreAlert::Lottery()
 
 void PreAlert::Update()
 {
+    if (!active) return;
     pre_posy += 5;
     if (pre_posy < 200)
     {
-        pre_posy += 20;
+        pre_posy = 200;
+        active = false;
+        end = true;
     }
 }
 
 void PreAlert::Draw()
 {
     DrawGraph(1280 / 2, pre_posy, pre_img, FALSE);
+}
+bool PreAlert::End()
+{
+    if (end)
+    {
+        end = false;
+        return true;
+    }
+
+    return false;
 }
--- PreAlert.h
+++ PreAlert.h
@@ -5,10 +5,15 @@ class PreAlert
 	int pre;
 	int pre_img;
 	int pre_posy;
+	bool active;
+	bool end;
 public:
 	void Init();
 
 	bool Lottery();
 	void Update();
 	void Draw();
+
+	void Start();
+	bool End();
 };

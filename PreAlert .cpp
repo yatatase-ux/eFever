40608359a81576c5b76f8722d9f33174d153e44c
4A-17\koyo <a>
2026-09-19 11:34:20 +0900

    pre画像をいれる

--- PreAlert .cpp
+++ PreAlert .cpp
@@ -4,6 +4,8 @@
 void PreAlert::Init()
 {
     pre = 20;
+    pre_img = LoadGraph("image/pre.png");
+    pre_posy = -300;
 }
 
 
@@ -11,4 +13,19 @@ bool PreAlert::Lottery()
 {
 
     return GetRand(99) < pre;
+}
+
+
+void PreAlert::Update()
+{
+    pre_posy += 5;
+    if (pre_posy < 200)
+    {
+        pre_posy += 20;
+    }
+}
+
+void PreAlert::Draw()
+{
+    DrawGraph(1280 / 2, pre_posy, pre_img, FALSE);
 }
--- PreAlert.h
+++ PreAlert.h
@@ -3,9 +3,12 @@
 class PreAlert
 {
 	int pre;
+	int pre_img;
+	int pre_posy;
 public:
 	void Init();
 
 	bool Lottery();
-
+	void Update();
+	void Draw();
 };
